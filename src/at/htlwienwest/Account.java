2c141e41d14eea182345f1dd4e9bf775c1a7cd31
chris <hartmann.christoph@gmx.at>
2020-12-16 19:31:15 +0100

    Added constructor and getter to class Account

--- src/at/htlwienwest/Account.java
+++ src/at/htlwienwest/Account.java
@@ -3,4 +3,17 @@ package at.htlwienwest;
 public class Account {
     int amount;
     int limit;
+
+    public Account(int amount, int limit) {
+        this.amount = amount;
+        this.limit = limit;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
 }

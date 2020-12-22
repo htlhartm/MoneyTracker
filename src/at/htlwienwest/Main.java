500cbb4b4070cd550f2995e9c2ef9a1383f42dc9
chris <hartmann.christoph@gmx.at>
2020-12-22 04:47:30 +0100

    Added alles ok?

--- src/at/htlwienwest/Main.java
+++ src/at/htlwienwest/Main.java
@@ -2,6 +2,7 @@ package at.htlwienwest;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hi, alles ok?");
         System.out.println("Hi, wie gehts?");
     }
 }

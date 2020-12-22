30e4c02f5fb18077f532c95242ff219c79e0de58
chris <hartmann.christoph@gmx.at>
2020-12-22 05:53:42 +0100

    Added Person

--- src/at/htlwienwest/Main.java
+++ src/at/htlwienwest/Main.java
@@ -1,9 +1,13 @@
 package at.htlwienwest;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Herzlich willkommen");
         System.out.println("Es freut mich sehr, dass ihr hier seid");
 
+        Person person = new Person("Peter", "M",
+                LocalDate.of(1990, 01, 01), "Hauptstrasse 1");
     }
 }

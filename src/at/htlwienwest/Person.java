9960d1369774b097f28fb5a4a171e07fd52e13ee
chris <hartmann.christoph@gmx.at>
2020-12-24 22:58:34 +0100

    deleted birthday in person

--- src/at/htlwienwest/Person.java
+++ src/at/htlwienwest/Person.java
@@ -5,13 +5,11 @@ import java.time.LocalDate;
 public class Person {
     private String firstName;
     private String lastName;
-    private LocalDate bithday;
     private String address;
 
-    public Person(String firstName, String lastName, LocalDate bithday, String address) {
+    public Person(String firstName, String lastName, String address) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.bithday = bithday;
         this.address = address;
     }
 
@@ -21,10 +19,6 @@ public class Person {
 
     public String getLastName() {
         return lastName;
-    }
-
-    public LocalDate getBithday() {
-        return bithday;
     }
 
     public String getAddress() {
